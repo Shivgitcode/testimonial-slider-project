6362ce030633df643dc1c181ed69c74765bf70cf
shivansh <shivneeraj2004@gmail.com>
2023-12-08 18:38:03 +0530

    added state in the project

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,4 @@
 import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
-import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 export default function Card(props) {
   let review = props.review;
   return (
@@ -26,19 +25,6 @@ export default function Card(props) {
       <div className="text-center mt-4 text-slate-500">{review.text}</div>
       <div className="text-violet-400 mx-auto mt-5">
         <FaQuoteRight></FaQuoteRight>
-      </div>
-      <div className="flex text-3xl mt-5 gap-3 text-violet-400 font-bold mx-auto">
-        <button className="cursor-pointer hover:text-violet-500">
-          <FiChevronLeft></FiChevronLeft>
-        </button>
-        <button className="cursor-pointer hover:text-violet-500">
-          <FiChevronRight></FiChevronRight>
-        </button>
-      </div>
-      <div>
-        <button className="bg-violet-400 hover:bg-violet-500 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg ">
-          surprise me
-        </button>
       </div>
     </div>
   );
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,60 @@
+import { useState } from "react";
 import Card from "./Card";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+// import reviews from "../data";
 
 const Testimonials = (props) => {
+  const [index, setIndex] = useState(0);
+  function leftShiftHandler() {
+    if (index - 1 < 0) {
+      setIndex(props.reviews.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  }
+
+  function rightShiftHandler() {
+    if (index + 1 >= props.reviews.length) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  }
+
+  function surpriseHandler() {
+    const newIndex = Math.floor(Math.random() * 4);
+    setIndex((prevIndex) => {
+      prevIndex = newIndex;
+      return prevIndex;
+    });
+  }
   return (
     <div>
-      <Card review={props.reviews[0]}></Card>
+      <Card review={props.reviews[index]}></Card>
+
+      <div className="flex text-3xl mt-5 gap-3 text-violet-400 font-bold mx-auto">
+        <button
+          className="cursor-pointer hover:text-violet-500"
+          onClick={leftShiftHandler}
+        >
+          <FiChevronLeft></FiChevronLeft>
+        </button>
+        <button
+          className="cursor-pointer hover:text-violet-500"
+          onClick={rightShiftHandler}
+        >
+          <FiChevronRight></FiChevronRight>
+        </button>
+      </div>
+      <div>
+        <button
+          className="bg-violet-400 hover:bg-violet-500 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg "
+          onClick={surpriseHandler}
+        >
+          surprise me
+        </button>
+      </div>
     </div>
   );
 };
